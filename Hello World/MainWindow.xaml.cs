cb015bac43ff507a67f7dd29d46128925e71ee4a
Sangmin <99625610+jsmin0131@users.noreply.github.com>
2022-03-10 17:15:55 +0900

    first commit

---
+++ Hello World/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P01_HelloWorld
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello " + nameBox.Text);
+        }
+
+        private void Button_Wow(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wow");
+        }
+    }
+}
