284847c9d66f157b743091c11728b2b9a0cb935a
Sangmin Jeong <99625610+jsmin0131@users.noreply.github.com>
2022-03-15 06:27:17 +0900

    ICommand Test

---
+++ ICommand_Test1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+//https://www.sysnet.pe.kr/2/0/10917
+
+namespace ICommand_Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new RelayCommand();
+
+            ConsolePrompt prompt = new ConsolePrompt();
+            prompt.Command = cmd;
+            prompt.Run();
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        // Execute를 실행할 수 있는가에 대한 조건이 변화했을때
+        public event EventHandler CanExecuteChanged;
+
+        //Execute를 실행할 수 있는가에 대한 조건을 설정할 수 있다.
+        public bool CanExecute(object parameter)
+        {
+            return DateTime.Now.Hour != 9;//9시에서 10시 사이에는 명령어처리를 할 수 없다. 할당값을 변화시켜가면서 테스트 가능
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine(DateTime.Now + ": RelayCommand.Execute - " + parameter);
+        }
+    }
+
+    public class ConsolePrompt
+    {
+        public ICommand Command
+        {
+            get; set;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string txt = Console.ReadLine();
+                if (string.IsNullOrEmpty(txt) == true)
+                    break;
+                //Command.Execute(txt);
+
+                if(Command.CanExecute(null) == true)
+                {
+                    Command.Execute(txt);
+                }
+            }
+        }
+    }
+}
---
+++ ICommand_Test2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ICommand_Test2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ICommand _clickCommand = new RelayCommand();
+        public ICommand ClickCommand
+        {
+            get { return _clickCommand; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClickCommand.CanExecuteChanged += CanExecuteChanged_testfunc;
+        }
+
+        private void _txtBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            (_clickCommand as RelayCommand).FireChanged(_txtBox.Text.Length != 0);
+        }
+
+        private void CanExecuteChanged_testfunc(object sender, EventArgs arg)
+        {
+            MessageBox.Show($"CanExecuteChanged_testfunc 실행! {_txtBox.Text.Length != 0}");
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        bool _canExceute = false;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("RelayCommand.Execute called!");
+        }
+
+        public void FireChanged(bool cond)
+        {
+            _canExceute = cond;
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+}
